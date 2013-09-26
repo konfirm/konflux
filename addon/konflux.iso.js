47387ac4b2bf24c00e9be55271cb924257e5ae1e
Rogier Spieker <rogier@konfirm.net>
2013-09-26 18:38:24 +0200

    Added kx.polygon and kx.iso addon modules

---
+++ addon/konflux.iso.js
@@ -0,0 +1,150 @@
+/*
+ *       __    Konflux - Isometric polygons (version $DEV$ - $DATE$)
+ *      /\_\
+ *   /\/ / /   Copyright 2012-2013, Konfirm (Rogier Spieker)
+ *   \  / /    Releases under the MIT license
+ *    \/_/     More information: http://konfirm.net/konflux
+ */
+
+/*jshint undef: true, unused: true, curly: false, newcap: false, forin: false, maxstatements: 10, maxparams: 4 */
+;(function(konflux){
+	"use strict";
+
+
+	/**
+	 *  Isometric shapes based on konflux.polygon
+	 *  @module  iso
+	 *  @note    available as konflux.iso / kx.iso
+	 *  @note    All public methods from konflux.polygon are inherited by konflux.iso (allowing konflux.iso to move, snap, scale, etc (all in 2d space!))
+	 */
+	function kxISO()
+	{
+		var iso = this,
+			arg = Array.prototype.slice.call(arguments),
+			poly = arg[0] instanceof konflux.polygon ? arg[0] : new konflux.polygon(arg);
+
+
+		/**
+		 *  Initialize the isometric polygon, inheriting all public methods konflux.polygon instances have
+		 *  @name    init
+		 *  @type    function
+		 *  @access  internal
+		 *  @return  void
+		 */
+		function init()
+		{
+			var p;
+
+			for (p in poly)
+				if (typeof poly[p] === 'function' && !(p in iso))
+					iso[p] = bridge(p);
+		}
+
+		/**
+		 *  Create a function which acts as bridge onto the konflux.polygon and ensures chainability
+		 *  @name    bridge
+		 *  @type    function
+		 *  @access  internal
+		 *  @param   string   method
+		 *  @return  function handler
+		 */
+		function bridge(method)
+		{
+			return function(){
+				var arg = Array.prototype.slice.call(arguments),
+					result = poly[method].apply(poly, arg);
+
+				if (result instanceof konflux.polygon)
+					result = result === poly ? iso : new kxISO(result);
+
+				return result;
+			};
+		}
+
+		/**
+		 *  Sort an array of konflux.polygons to their most likely Z-axis (assumes the same Z-axis voor all polygons)
+		 *  @name    zOrder
+		 *  @type    function
+		 *  @access  internal
+		 *  @param   Array  konflux.polygon
+		 *  @return  Array  konflux.polygon sorted
+		 */
+		function zOrder(polyList)
+		{
+			polyList.sort(function(a, b){
+				var pa = a.points()[0].min.apply(null, a),
+					pb = b.points()[0].min.apply(null, b);
+
+				return (pa.x + pa.y) - (pb.x + pb.y);
+			});
+			return polyList;
+		}
+
+		/**
+		 *  Draw the konflux.iso onto given canvas
+		 *  @name    draw
+		 *  @type    method
+		 *  @access  public
+		 *  @param   kxCanvas  canvas
+		 *  @param   string    color [optional, default rgb(255, 128, 0) - orange]
+		 *  @param   number    isometric angle [optional, default 30 degrees - the most common angle]
+		 *  @param   number    height [optional, default 20]
+		 *  @return  kxISO     iso
+		 */
+		iso.draw = function(canvas, color, iso, height)
+		{
+			var shape = iso ? poly.iso(iso || 30) : poly.clone(),
+				buffer = {left:[], right:[]},
+				points = shape.points(),
+				length = points.length,
+				i, j;
+
+			height = height ? height : (iso ? Math.min(iso * .2, 16) : false);
+
+			if (height)
+			{
+				for (i = 0; i < length; ++i)
+				{
+					j = (i + 1) % length;
+					buffer[Math.floor(Math.abs(points[i].angle(points[j]))) === 2 ? 'left' : 'right'].push(new konflux.polygon(
+						points[i],
+						points[j],
+						points[j].clone().move(0, height),
+						points[i].clone().move(0, height)
+					));
+				}
+				for (j in buffer)
+				{
+					buffer[j] = zOrder(buffer[j]);
+					for (i in buffer[j])
+						buffer[j][i]
+							.draw(canvas, color)
+							.draw(canvas, j === 'left' ? 'rgba(0,0,0,.2)' : 'rgba(0,0,0,.1)')
+						;
+				}
+			}
+
+			shape.draw(canvas, color);
+
+			return iso;
+		};
+
+		/**
+		 *  Clone the konflux.iso and return the newly created clone
+		 *  @name    clone
+		 *  @type    method
+		 *  @access  public
+		 *  @return  kxISO iso
+		 */
+		iso.clone = function()
+		{
+			return new kxISO(poly.clone());
+		};
+
+		init();
+	}
+
+	//  register kxISO as konflux.iso
+	konflux.iso = kxISO;
+
+})(window.konflux);
---
+++ addon/konflux.polygon.js
@@ -0,0 +1,159 @@
+/*
+ *       __    Konflux - Polygons (version $DEV$ - $DATE$)
+ *      /\_\
+ *   /\/ / /   Copyright 2012-2013, Konfirm (Rogier Spieker)
+ *   \  / /    Releases under the MIT license
+ *    \/_/     More information: http://konfirm.net/konflux
+ */
+
+/*jshint undef: true, unused: true, curly: false, newcap: false, forin: false, maxstatements: 10, maxparams: 4 */
+;(function(konflux){
+	"use strict";
+
+
+	/**
+	 *  Polygon shapes based on konflux.points
+	 *  @module  polygon
+	 *  @note    available as konflux.polygon / kx.polygon
+	 *  @note    All public methods from konflux.point are inherited by konflux.polygon (allowing konflux.polygon to move, snap, scale, etc)
+	 */
+	function kxPolygon()
+	{
+		var polygon = this,
+			points = arguments.length && arguments[0] instanceof Array ? arguments[0] : Array.prototype.slice.call(arguments);
+
+
+		/**
+		 *  Initialize the polygon, inheriting all public methods konflux.point instances have
+		 *  @name    init
+		 *  @type    function
+		 *  @access  internal
+		 *  @return  void
+		 */
+		function init()
+		{
+			var p;
+
+			for (p in points[0])
+				if (typeof points[0][p] === 'function' && !(p in polygon))
+					polygon[p] = bridge(p);
+		}
+
+		/**
+		 *  Create a function which acts as bridge onto all konflux.points within konflux.polygon and ensures chainability
+		 *  @name    bridge
+		 *  @type    function
+		 *  @access  internal
+		 *  @param   string   method
+		 *  @return  function handler
+		 */
+		function bridge(method)
+		{
+			return function(){
+				var arg = Array.prototype.slice.call(arguments),
+					result = batch.apply(polygon, [method].concat(arg));
+
+				if ('length' in result && result.length > 0 && result[0] instanceof konflux.point)
+					result = result[0] === points[0] ? polygon : new kxPolygon(result);
+
+				return result;
+			};
+		}
+
+		/**
+		 *  Call a method with the same arguments on all konflux.points within the konflux.polygon instance and return
+		 *  their individual values in an array
+		 *  @name    batch
+		 *  @type    function
+		 *  @access  internal
+		 *  @param   string  method
+		 *  @param   mixed   arg 1
+		 *  @param   mixed   ...
+		 *  @param   mixed   arg N
+		 *  @return  Array   result
+		 */
+		function batch()
+		{
+			var arg = Array.prototype.slice.call(arguments),
+				method = arg.shift(),
+				result = [],
+				i;
+
+			for (i = 0; i < points.length; ++i)
+				result.push(points[i][method].apply(points[i], arg));
+
+			return result;
+		}
+
+		/**
+		 *  Close the polygon by ensuring the last konflux.point equals the first
+		 *  @name    close
+		 *  @type    method
+		 *  @access  public
+		 *  @return  kxPolygon polygon
+		 */
+		polygon.close = function()
+		{
+			//  make sure we have a closed shape, if the last point is not equal to the first, we clone it and add it to the stack
+			if (!points[0].equal(points[points.length - 1]))
+				points.push(points[0].clone());
+
+			return polygon;
+		};
+
+		/**
+		 *  Obtain all konflux.point instances within the konflux.polygon instance
+		 *  @name    points
+		 *  @type    method
+		 *  @access  public
+		 *  @return  Array  konflux.points
+		 */
+		polygon.points = function()
+		{
+			return points;
+		};
+
+		/**
+		 *  Draw the konflux.polygon onto given canvas
+		 *  @name    draw
+		 *  @type    method
+		 *  @access  public
+		 *  @param   kxCanvas  canvas
+		 *  @param   string    color [optional, default rgb(255, 128, 0) - orange]
+		 *  @return  kxPolygon polygon
+		 */
+		polygon.draw = function(canvas, color)
+		{
+			canvas
+				.fillStyle(color || 'rgb(255,128,0)')
+				.line(points)
+				.fill()
+			;
+
+			return polygon;
+		};
+
+		/**
+		 *  Clone the konflux.polygon and return the newly created clone
+		 *  @name    clone
+		 *  @type    method
+		 *  @access  public
+		 *  @return  kxPolygon polygon
+		 */
+		polygon.clone = function()
+		{
+			var clone = [],
+				i;
+			for (i = 0; i < points.length; ++i)
+				clone.push(points[i].clone());
+
+			return new kxPolygon(clone);
+		};
+
+		init();
+	}
+
+	//  register the polygon library in konflux
+	konflux.polygon = kxPolygon;
+
+})(window.konflux);
