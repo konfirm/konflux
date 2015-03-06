fe857f47a2c214a3996657bdb71bbaccd9e2f06e
Rogier Spieker <rogier@konfirm.net>
2015-03-06 13:13:49 +0100

    Figured out why the kx.point method remained uncovered by the unit tests
    In order to count for code coverage, the code has to be executed by code from within an 'it' statement, not the 'describe' statement

--- test/spec/konflux.point.js
+++ test/spec/konflux.point.js
@@ -1,12 +1,9 @@
 describe('Konflux.point', function(){
 
-	var point = kx.point(10, 100);
-
 	it('point ({x:10, y:100}) x = 10', function(){
-		expect(point.x).toEqual(10);
-	});
+		var point = kx.point(10, 100);
 
-	it('point ({x:10, y:100}) y = 100', function(){
+		expect(point.x).toEqual(10);
 		expect(point.y).toEqual(100);
 	});
 
