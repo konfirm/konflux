ab567651a08fa6b3c458fce9281a320d76892695
Rogier Spieker <rogier@konfirm.net>
2015-05-14 13:28:45 +0200

    working out issues with source maps by moving to a simpler gulp setup. Also switching file size plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 'use strict';
 
+/*jshint browser:false, node:true*/
+
+/*
 var project = require('./gulp/project'),
 	konflux = [
 		'./src/konflux.js',
-		'./src/core/**/*.js'
+		'./src/core/ * * /*.js'
 	],
 	notkonflux = konflux.map(function(file) {
 		return '!' + file;
@@ -19,11 +22,11 @@ var project = require('./gulp/project'),
 		},
 
 		script: {
-			watch: notkonflux.concat(['./src/**/*.js']),
-			build: notkonflux.concat(['./src/*/*.js'])
+			watch: notkonflux.concat(['./src/ ** / *.js']),
+			build: notkonflux.concat(['./src/ * / *.js'])
 		},
 
-		syntax: './src/**/*.js'
+		syntax: './src/ * * / *.js'
 	};
 
 Object.keys(config).forEach(function(task) {
@@ -31,3 +34,92 @@ Object.keys(config).forEach(function(task) {
 });
 
 project.start('konflux', 'script', 'syntax');
+*/
+
+var gulp = require('gulp'),
+	plumber = require('gulp-plumber'),
+	include = require('gulp-include'),
+	uglify = require('gulp-uglify'),
+	sourcemaps = require('gulp-sourcemaps'),
+	rename = require('gulp-rename'),
+	replace = require('gulp-replace'),
+	jscs = require('gulp-jscs'),
+	jshint = require('gulp-jshint'),
+	size = require('gulp-filesize'),
+	del = require('del');
+
+gulp.task('konflux', function() {
+	gulp.src([
+		'./src/konflux.js'
+	])
+		.pipe(plumber())
+		.pipe(sourcemaps.init())
+		.pipe(include())
+		.pipe(gulp.dest('./build', {read: true}))
+		.pipe(size())
+
+		.pipe(uglify())
+		.pipe(rename(function(file) {
+			file.basename += '.min';
+		}))
+		.pipe(sourcemaps.write('./'))
+		.pipe(size())
+		.pipe(gulp.dest('./build'))
+	;
+});
+
+gulp.task('scripts', function() {
+	gulp.src([
+		'!./src/konflux.js',
+		'!./src/core/**/*.js',
+		'./src/*/*.js'
+	])
+		.pipe(plumber())
+		.pipe(sourcemaps.init())
+		.pipe(rename(function(file) {
+			file.basename = 'konflux.' + file.basename;
+		}))
+		.pipe(include())
+		.pipe(gulp.dest('./build', {read: true}))
+		.pipe(size())
+
+		.pipe(uglify())
+		.pipe(rename(function(file) {
+			file.basename += '.min';
+		}))
+		.pipe(sourcemaps.write('./'))
+		.pipe(size())
+		.pipe(gulp.dest('./build'))
+	;
+});
+
+gulp.task('syntax', function() {
+	gulp.src('./src/**/*.js')
+		.pipe(jscs())
+	;
+});
+
+gulp.task('clean', function(done) {
+	del(['./build/*'], done);
+});
+
+gulp.task('watch', function() {
+	gulp.watch([
+		'./src/konflux.js',
+		'./src/core/**/*.js'
+	], [
+		'konflux'
+	]);
+
+	gulp.watch([
+		'!./src/konflux.js',
+		'!./src/core/**/*.js',
+		'./src/**/*.js'
+	], [
+		'scripts'
+	]);
+
+	gulp.watch(['./src/**/*.js'], ['syntax']);
+});
+
+gulp.task('default', ['clean', 'watch', 'konflux', 'scripts', 'syntax']);
