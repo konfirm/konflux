59d761492b309c62f5756e021ea942eb6695a3ad
Rogier Spieker <rogier@konfirm.net>
2015-05-18 15:25:09 +0200

    fixed issue with special builds not getting a proper name

--- gulp/task/konflux.js
+++ gulp/task/konflux.js
@@ -8,24 +8,24 @@ module.exports = function(stream, devour, name) {
 
 	if (match) {
 		if (match[2]) {
-			console.log('Building special "%s" version with minial dependencies!', match[2]);
 			list = match[2].split(/[,\s]+/).map(function(key) {
 				return 'src/core/' + key;
 			});
-
-			suffix = list.join('-');
+			suffix = match[2].replace(/[^a-z]+/i, '-');
 		}
 	}
 
 	return stream
+		//  'compile' the full files and check for changes
+		.pipe(devour.pipe('compile', list))
+
+		//  rename the file if we are dealing with a special compilation
 		.pipe(devour.plugin('rename', function(file) {
 			if (suffix) {
 				file.basename += '-' + suffix;
+				console.log('Building special "%s" version, saved as %s', match[2], file.basename + file.extname);
 			}
 		}))
-
-		//  'compile' the full files and check for changes
-		.pipe(devour.pipe('compile', list))
 
 		//  write the full source to the output directory
 		.pipe(devour.write())
