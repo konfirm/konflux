f8f7a4a25240927be448eba6b5976cbc7389679d
Rogier Spieker <rogier@konfirm.net>
2015-05-18 14:35:31 +0200

    Complete overhaul of the embed logic, now also resolves dependencies

--- gulp/pipe/embed.js
+++ gulp/pipe/embed.js
@@ -1,125 +1,231 @@
 /*jshint node:true*/
 'use strict';
 
-var fs = require('fs'),
-	through = require('through2'),
-	embed = /([\t ]*)\/\/@embed:?\s*([a-z0-9_\-\.\/]+)/g;
+function Embed(devour, build) {
+	var embed = this,
+		fs = require('fs'),
+		through = require('through2'),
+		pattern = {
+			//  declarative
+			include: /([\t ]*)\/\/@(include|register):?\s*([a-z0-9_\-\.\/]+)\n/g,
+			depend: /@depend:?\s(.*)/g,
+			modules: /([\t ]*)\/\/@modules/,
 
-function unwrap(content) {
-	var header = /^;?\(function\([^\)]*\)\s*\{\s*(['"])use strict\1;?\n+/,
-		footer = /\n+\s*\}\)\([^\)]*\);?\n+$/,
-		indent;
+			//  sanitation
+			header: /^;?\(function\([^\)]*\)\s*\{(?:\s*\/\/.*)?\s*(['"])use strict\1;?\n+/,
+			footer: /\n+\s*\}\)\([^\)]*\);?\n+$/,
+			indentation: /^(?:\n+?)?([\s]+)/,
 
-	//  remove header and footer only if both are present in the expected format
-	if (header.test(content) && footer.test(content)) {
-		content = content.replace(header, '').replace(footer, '').split(/\n/).map(function(line) {
-			var match;
+			// convenience
+			separator: /[,\s]+/,
+			js: /\.js$/
+		},
+		list = {};
 
-			if (typeof indent === 'undefined') {
-				match = line.match(/^(?:\n+?)?([\s]+)/);
+	function processBuffer(buffer) {
+		var content = buffer.contents.toString(),
+			dependencies;
 
-				if (match) {
-					indent = new RegExp('^' + match[1]);
+		content = resolve(buffer.contents.toString(), buffer.path);
+		dependencies = getDependencies(build, build && build.length > 0);
+
+		if (dependencies.length) {
+			buffer.contents = new Buffer(content.replace(pattern.modules, function() {
+				return dependencies.map(function(dep) {
+
+					return dep.content;
+				}).join('\n');
+			}));
+		}
+	}
+
+	function getDependencies(requires, announce) {
+		var changes = true,
+			keys = Object.keys(list),
+			deps = keys.filter(function(key) {
+				return !list[key].included && (requires.length <= 0 || requires.indexOf(key) >= 0);
+			}),
+			iteration = 0,
+			append;
+
+		while (changes) {
+			append = [];
+			++iteration;
+
+			deps.forEach(function(dep) {
+				keys.filter(function(key) {
+					return 'dependant' in list[key].type && list[key].type.dependant.indexOf(dep) >= 0 && deps.indexOf(key) < 0;
+				}).forEach(function(key) {
+					if (append.indexOf(key) < 0) {
+						if (announce) {
+							console.log('%s ++ dependency: %s', new Array(iteration + 1).join(''), key);
+						}
+
+						append.push(key);
+					}
+				});
+			});
+
+			changes = append.length;
+			if (changes) {
+				deps = deps.concat(append);
+			}
+		}
+
+		return deps.map(function(dep) {
+			return list[dep];
+		});
+	}
+
+	function resolve(content, origin, indent) {
+		var base = getProjectOffset(origin),
+			baseDir = base.split('/').slice(0, -1).join('/') + '/';
+
+		//  process includes and registers
+		content = content.replace(pattern.include, function(match, indentation, type, files) {
+			var result = files.split(pattern.separator).map(function(file) {
+					var filename = process.cwd() + '/' + baseDir + file + (pattern.js.test(file) ? '' : '.js'),
+						start = process.hrtime(),
+						module = include(getProjectOffset(filename).replace(pattern.js, ''), base, type),
+						data;
+
+					if (!module.content) {
+						data = resolve(
+							fs.readFileSync(filename).toString(),
+							baseDir + file,
+							indentation
+						);
+
+						data = unwrap(data, (indent || '') + indentation);
+
+						module.content = [
+							indentation + '//BEGIN INCLUDE: ' + file,
+							data.split(/\n/).map(function(line) {
+								return line ? indentation + line : '';
+							}).join('\n'),
+							'',
+							indentation + '//END INCLUDE: ' + file + ' [' + report(start, data.length) + ']'
+						].join('\n');
+					}
+
+					if (type === 'include' && !module.included) {
+						module.included = true;
+						return module.content;
+					}
+
+					return '';
+				}).join('');
+
+			return (result ? '\n' : '') + result;
+		});
+
+		//  extract dependencies
+		content = content.replace(pattern.depend, function(match, files) {
+			files.split(pattern.separator).forEach(function(file) {
+				include(getProjectOffset('src/core/' + file), origin, 'dependant');
+			});
+
+			//  remove them from the output
+			return '';
+		});
+
+		return content;
+	}
+
+	function unit(value, step, units, dec) {
+		var list = units.slice(),
+			result = +value;
+
+		while (result > step && list.length > 1) {
+			list.shift();
+			result /= step;
+		}
+
+		return result.toFixed(dec || 2) + list.shift();
+	}
+
+	function report(start, size) {
+		var end = process.hrtime(start);
+
+		return [
+			unit(end[0] * 1e6 + end[1] / 1e3, 1000, ['µs', 'ms', 's']),
+			', ',
+			unit(size, 1024, ['bytes', 'KB', 'MB'])
+		].join('');
+	}
+
+	function unwrap(content) {
+		var indent;
+
+		//  remove header and footer only if both are present in the expected format
+		if (pattern.header.test(content) && pattern.footer.test(content)) {
+			content = content.replace(pattern.header, '').replace(pattern.footer, '').split(/\n/).map(function(line) {
+				var match;
+
+				if (typeof indent === 'undefined') {
+					match = line.match(pattern.indentation);
+
+					if (match) {
+						indent = new RegExp('^' + match[1]);
+					}
 				}
-			}
 
-			return line.replace(indent, '');
-		}).join('\n');
-	}
-
-	return content.replace(/^\n+|\n+$/, '');
-}
-
-function getProjectOffset(path) {
-	var self = __dirname.split('/'),
-		base = path.split('/'),
-		same = true;
-
-	return base.filter(function(part, index) {
-		if (same && part !== self[index]) {
-			same = false;
+				return line.replace(indent, '');
+			}).join('\n');
 		}
 
-		return !same;
-	}).join('/');
-}
-
-function report(start, size) {
-	var end = process.hrtime(start),
-		time = ['µs', 'ms', 's'],
-		byte = ['bytes', 'KB', 'MB'],
-		t, b;
-
-	end = end[0] * 1e6 + end[1] / 1e3;
-
-	while (end > 1000 && time.length > 1) {
-		time.shift();
-		end /= 1000;
+		return content.replace(/^\n+|\n+$/, '');
 	}
 
-	while (size > 1024 && byte.length > 1) {
-		byte.shift();
-		size /= 1024;
-	}
+	function getProjectOffset(path) {
+		var self = __dirname.split('/'),
+			base = path.split('/'),
+			same = true;
 
-	return [
-		end.toFixed(2), time.shift(),
-		', ',
-		size.toFixed(2), byte.shift()
-	].join('');
-}
-
-function resolve(content, base, indent) {
-	var path = getProjectOffset(base);
-
-	return content.replace(embed, function(match, indentation, files) {
-		return '\n' + files.split(/[,\s]+/).map(function(file) {
-			var partial = file.split('/'),
-				start = process.hrtime(),
-				data, end;
-
-			if (!/\.js$/.test(file)) {
-				file += '.js';
+		return base.filter(function(part, index) {
+			if (same && part !== self[index]) {
+				same = false;
 			}
 
-			data = resolve(
-				unwrap(
-					fs.readFileSync(base + file).toString(),
-					(indent || '') + indentation
-				),
-				base + partial[0] + '/',
-				indentation
-			);
+			return !same;
+		}).join('/');
+	}
 
-			end = process.hrtime(start);
-
-			return [
-				indentation + '//BEGIN EMBED: ' + path + file,
-				data.split('\n').map(function(line) {
-					return line ? indentation + line : '';
-				}).join('\n'),
-				'',
-				indentation + '//END EMBED: ' + path + file + ' [' + report(start, data.length) + ']'
-			].join('\n');
-		}).join('');
-	});
-}
-
-function checkDeclarations() {
-	return through.obj(function(chunk, enc, done) {
-		var path = chunk.path.split('/');
-
-		if (chunk.isBuffer()) {
-			chunk.contents = new Buffer(resolve(chunk.contents.toString(), path.slice(0, -1).join('/') + '/'));
+	function include(name, module, type) {
+		if (!(name in list)) {
+			list[name] = {
+				name: name,
+				content: null,
+				type: {},
+				included: false
+			};
 		}
 
-		this.push(chunk);
-		done();
-	});
+		if (!(type in list[name].type)) {
+			list[name].type[type] = [];
+		}
+
+		list[name].type[type].push(module.replace(pattern.js, ''));
+
+		return list[name];
+	}
+
+	embed.resolve = function() {
+		return through.obj(function(chunk, enc, done) {
+			if (chunk.isBuffer()) {
+				processBuffer(chunk);
+			}
+
+			this.push(chunk);
+			done();
+		});
+	};
 }
 
-module.exports = function(stream, devour) {
+module.exports = function(stream, devour, list) {
+	var embed = new Embed(devour, list);
+
 	return stream
-		.pipe(checkDeclarations(devour))
+		.pipe(embed.resolve())
 	;
 };
